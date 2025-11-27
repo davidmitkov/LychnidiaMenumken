fa7a2e938f54ec8e47b567ffbe6e6e065c23ad3c
davidmitkov <mitkovdavid@gmail.com>
2025-11-28 00:38:26 +0100

    Update appturkey.js

--- appturkey.js
+++ appturkey.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
         id: 0,
       },
       {
-        img: "images/beer.jpeg",
+        img: "images/pivo3.jpg",
         name: "Bira",
         id: 1,
       },
@@ -21,79 +21,94 @@ document.addEventListener("DOMContentLoaded", function() {
         id: 3,
       },
       {
-        img: "images/alcochol.jpg",
-        name: "Alkollü İçecekler",
+        img: "images/rakija.jpg",
+        name: "RAKIJA",
         id: 4,
       },
       {
-        img: "images/salad.jpg",
-        name: "Salatalar",
+        img: "images/jack.jpg",
+        name: "Alkollü İçecekler",
         id: 5,
       },
       {
+        img: "images/sopska.jpg",
+        name: "Salatalar",
+        id: 6,
+      },
+      {
+        "img": "images/daski.png",
+        "name": "SUNUM TAHTALARI",
+        "id": 7
+      },      
+      {
         img: "images/breakfast.jpg",
         name: "Kahvaltı ve Brunch",
-        id: 6,
+        id: 8,
       },
       {
         img: "images/mincemeat.jpg",
         name: "Kıyma Yemekleri",
-        id: 7,
+        id: 9,
       },
       {
         img: "images/vealmeat.jpg",
         name: "Dana Eti Yemekleri",
-        id: 8,
+        id: 10,
       },
       {
         img: "images/porkmeat.jpg",
         name: "Domuz Eti Yemekleri",
-        id: 9,
+        id: 11,
       },
       {
         img: "images/chickenmeat.jpg",
         name: "Tavuk Eti Yemekleri",
-        id: 10,
+        id: 12,
       },
       {
         img: "images/tavafood.jpg",
         name: "Tava Yemekleri",
-        id: 11,
+        id: 13,
       },
       {
-        img: "images/burger.jpg",
-        name: "Burgerler",
-        id: 12,
+        img: "images/musaka.jpg",
+        name: "MAKEDONYA GELENEKSEL YEMEKLERİ",
+        id: 14,
       },
       {
         img: "images/pasta.jpg",
         name: "Risotto ve Makarna",
-        id: 13,
+        id: 15,
       },
       {
         img: "images/speciality.jpg",
         name: "Uzmanlık Yemekleri",
-        id: 14,
+        id: 16,
       },
       {
         img: "images/fish.jpg",
         name: "Balık",
-        id: 15,
+        id: 17,
       },
       {
         img: "images/dessert.jpg",
         name: "Tatlılar",
-        id: 16,
+        id: 18,
       },
       {
         img: "images/peppers.jpg",
         name: "Ekstralar",
-        id: 17,
+        id: 19,
       },
       {
         img: "images/cocktails.jpg",
         name: "Kokteyller",
-        id: 18,
+        id: 20,
+      },
+      {
+        img: "images/extraoffer.png",
+        name: "EKSTRA TEKLİF",
+        id: 21,
       },
     ]
     
